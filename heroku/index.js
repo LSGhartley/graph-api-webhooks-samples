dbda442ceaed6c5977b579e6f31e68daf835a9d1
LSGhartley <98523493+LSGhartley@users.noreply.github.com>
2024-08-12 19:38:53 +0200

    Adding ability to send a message

--- heroku/index.js
+++ heroku/index.js
@@ -6,53 +6,82 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-var bodyParser = require('body-parser');
-var express = require('express');
+var bodyParser = require("body-parser");
+var express = require("express");
 var app = express();
-var xhub = require('express-x-hub');
+var xhub = require("express-x-hub");
+const axios = require("axios");
 
-app.set('port', (process.env.PORT || 5000));
-app.listen(app.get('port'));
+app.set("port", process.env.PORT || 5000);
+app.listen(app.get("port"));
 
-app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
+app.use(xhub({ algorithm: "sha1", secret: process.env.APP_SECRET }));
 app.use(bodyParser.json());
 
-var token = process.env.TOKEN || 'token';
+var token = process.env.TOKEN || "token";
 var received_updates = [];
 
-app.get('/', function(req, res) {
+app.get("/", function (req, res) {
   console.log(req);
-  res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
+  res.send("<pre>" + JSON.stringify(received_updates, null, 2) + "</pre>");
 });
 
-app.get(['/facebook', '/instagram'], function(req, res) {
+app.get(["/facebook", "/instagram"], function (req, res) {
   if (
-    req.query['hub.mode'] == 'subscribe' &&
-    req.query['hub.verify_token'] == token
+    req.query["hub.mode"] == "subscribe" &&
+    req.query["hub.verify_token"] == token
   ) {
-    res.send(req.query['hub.challenge']);
+    res.send(req.query["hub.challenge"]);
   } else {
     res.sendStatus(400);
   }
 });
 
-app.post('/facebook', function(req, res) {
-  console.log('Facebook request body:', req.body);
+app.post("/facebook", async function (req, res) {
+  console.log("Facebook request body:", req.body);
 
   if (!req.isXHubValid()) {
-    console.log('Warning - request header X-Hub-Signature not present or invalid');
+    console.log(
+      "Warning - request header X-Hub-Signature not present or invalid"
+    );
     res.sendStatus(401);
     return;
   }
 
-  console.log('request header X-Hub-Signature validated');
+  console.log("request header X-Hub-Signature validated");
   // Process the Facebook updates here
   received_updates.unshift(req.body);
   res.sendStatus(200);
+  const {
+    entry: {
+      changes: {
+        messages: { from },
+      },
+    },
+  } = req.body;
+
+  try {
+    const response = await axios.post(
+      `https://graph.facebook.com/${process.env.API_VERSION}/${process.env.BUSINESS_WA_ID}/message`,
+      {
+        messaging_product: "whatsapp",
+        to: "27659951223",
+        template: {
+          name: "welcome",
+          language: {
+            code: "en_US",
+          },
+        },
+      }
+    );
+    res.status(200).send("Message sent successfully");
+  } catch (error) {
+    res.status(500).send("Failed response");
+  }
 });
 
-app.post('/instagram', function(req, res) {
-  console.log('Instagram request body:');
+app.post("/instagram", function (req, res) {
+  console.log("Instagram request body:");
   console.log(req.body);
   // Process the Instagram updates here
   received_updates.unshift(req.body);
