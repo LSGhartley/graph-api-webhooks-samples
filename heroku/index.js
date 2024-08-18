8c01eebd0de670917c705e93136e660f37dd9e20
LSGhartley <98523493+LSGhartley@users.noreply.github.com>
2024-08-18 17:14:47 +0200

    adde env

--- heroku/index.js
+++ heroku/index.js
@@ -5,6 +5,7 @@
  * This source code is licensed under the license found in the
  * LICENSE file in the root directory of this source tree.
  */
+require("dotenv").config();
 
 var bodyParser = require("body-parser");
 var express = require("express");
